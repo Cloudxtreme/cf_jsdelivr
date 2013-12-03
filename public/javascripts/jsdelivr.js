7af3cd14f0bd44cedad6922a4b0027edeb6a0129
Dmitriy Akulov <dakulovgr@gmail.com>
2013-12-04 01:37:43 +0200

    testing version change

--- public/javascripts/jsdelivr.js
+++ public/javascripts/jsdelivr.js
@@ -5,20 +5,19 @@ CloudFlare.define('jsdelivr', ['jsdelivr/config'],
             mootools: 'cdn.jsdelivr.net/mootools/1.4.5/mootools-core-1.4.5-full-compat.js',
             backbone: 'cdn.jsdelivr.net/backbonejs/1.1.0/backbone-min.js',
             underscore: 'cdn.jsdelivr.net/underscorejs/1.5.2/underscore-min.js',
-            jquerycookie: 'cdn.jsdelivr.net/jquery.cookie/1.4.0/jquery.cookie.min.js',
+            jquerycookie: 'cdn.jsdelivr.net/jquery.cookie/1.3/jquery.cookie.js',
             mustache: 'cdn.jsdelivr.net/mustache.js/0.7.3/mustache.min.js'
         };
 
-        Object.keys(libs).forEach(function(id) {
-            // load the libs here
-            if(config[id] == 'true'){
+		Object.keys(libs).forEach(function(id) {
+        // load the libs here
+			if(config[id] == 'true'){
 				var a = document.createElement('script');
 				a.type = 'text/javascript';
 				a.src = '//' + libs[id];
 				document.body.appendChild(a);
-			}
+			}			
 			
-			
-        });
+		});
     }
 );
